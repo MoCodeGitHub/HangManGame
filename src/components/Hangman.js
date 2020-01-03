0db0b1807996badd7822388bc7761356ca9e9092
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-03 17:30:56 +0100

    Imported images

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import './Hangman.css';
 import { guessword } from './guessword.js';
 
+import step0 from  './images/0.jpg';
+import step1 from  './images/1.jpg';
+import step2 from  './images/2.jpg';
+import step3 from  './images/3.jpg';
+import step4 from  './images/4.jpg';
+import step5 from  './images/5.jpg';
+import step6 from  './images/6.jpg';
+
+
+
 class Hangman extends Component{
   render () {
     return (
