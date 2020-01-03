567c21e8d5bf0b8177ca70c736cf454e0e0bbdd6
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-03 17:20:04 +0100

    Test environment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hangman from './components/Hangman.js';
 
 function App() {
   return (
     <div className="App">
-
+      <Hangman />
     </div>
   );
 }
---
+++ src/components/Hangman.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Hangman.css';
+import { guessword } from './guessword.js';
+
+class Hangman extends Component{
+  render () {
+    return (
+      <div>
+      <h1> Hangman </h1>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default Hangman;
